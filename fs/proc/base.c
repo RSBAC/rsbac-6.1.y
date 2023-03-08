911428661a97763a484f96bfb972ca50887a80d6
Amon Ott <ao@rsbac.org>
2023-03-08 14:47:18 +0100

    Review: fix R_NET_SHUTDOWN in __sys_shutdown() and several minor glitches.

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -513,8 +513,7 @@ static int proc_pid_wchan(struct seq_file *m, struct pid_namespace *ns,
 					A_none,
 					rsbac_attribute_value)) {
 			put_pid(rsbac_target_id.process);
-			seq_putc(m, '0');
-			return 0;
+			goto print0;
 		}
 		put_pid(rsbac_target_id.process);
 	}
