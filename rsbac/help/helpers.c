d8ece0f63bab3f45b977058e1f137cc8445fd69b
Amon Ott <ao@rsbac.org>
2024-08-20 08:54:25 +0200

    With CAP fd hiding, do not hide from owner to avoid unaccessible objects.

--- rsbac/help/helpers.c
+++ rsbac/help/helpers.c
@@ -513,6 +513,9 @@ rsbac_boolean_t rsbac_cap_hide_fd(struct dentry * target_dentry)
 #endif
 		return FALSE;
 	}
+
+	if (uid_eq(target_dentry->d_inode->i_uid, current_fsuid()))
+		return FALSE;
 	if (!generic_permission(&init_user_ns, target_dentry->d_inode, MAY_READ))
 		return FALSE;
 
